01840b7967b2b3bd91aa63eab1deff8697bae531
AlicinhaDavid <alicedaviddossantos@gmail.com>
2023-02-14 11:36:09 -0300

    refactor: move the material app to a new class to make hot reload possible

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,18 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(ByteBankApp());
+}
+
+class ByteBankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         body: FormularioTransferencia(),
       ),
-    ),
-  );
+    );
+  }
 }
 
 class FormularioTransferencia extends StatelessWidget {
